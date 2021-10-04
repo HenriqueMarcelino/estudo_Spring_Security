a2bf1c3513a3f16227b44d6ecac2e55c68be665f
HenriqueMarcelino <henrique.marcelino1996@gmail.com>
2021-10-04 08:33:09 -0300

    Estudo segurança em Spring

---
+++ demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -0,0 +1,77 @@
+package com.mballem.curso.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mballem.curso.security.service.MedicoService;
+import com.mballem.curso.security.service.UsuarioService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	/*Configuração para trabalhar com security, aqui o proprio security
+	 * está bloqueando o css da pagina e outros para que não seja acessado 
+	 * sem estarmos na url correta que seria a inicial "home".
+	 * 
+	 * Estamos liberando os webjars/ o "**" é para liberar tudo que vem depois 
+	 * daquela barra.
+	 * 
+	 * Aqui também estamos fazendo a validação de senhas, e fazendo a criptografia delas.
+	 * 
+	 */
+	
+	@Autowired
+	private UsuarioService service;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	
+		http.authorizeRequests()
+		//Acessos Publicos liberados.
+			.antMatchers("/webjars/**", "/css/**", "/image/**", "/js/** ").permitAll()
+			.antMatchers("/", "/home").permitAll()
+			
+			// Estamos definido as permiçoes, cada usuario que conter, exemplo /u/alguma coisa 
+			// so vai pode acessar aquele perfil e não o do outro que seria medico/... 
+			
+			// ACESSOS PRIVADOS ADMIN
+			.antMatchers("/u/**").hasAuthority("ADMIN")
+			
+			// ACESSOS PRIVADOS MEDICOS
+			.antMatchers("/medicos/dados","/medicos/salvar","/medicos/editar").hasAnyAuthority("MEDICO,ADMIN")
+			.antMatchers("/medicos/**").hasAuthority("MEDICO")
+			
+			// ACESSOS PRIVADOS PACIENTE
+			.antMatchers("/paciente/**").hasAuthority("PACIENTE")
+			
+			// ACESSOS PRIVADOS ESPECIALIDADES
+			.antMatchers("/especialidades/datatables/server/medico/*").hasAnyAuthority("MEDICO,ADMIN")
+			.antMatchers("/especialidades/titulo").hasAnyAuthority("MEDICO,ADMIN")
+			.antMatchers("/especialidades/**").hasAuthority("ADMIN")
+			
+			.anyRequest().authenticated()
+			.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/", true)
+				.failureUrl("/login-error")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutSuccessUrl("/")
+			.and()
+				.exceptionHandling()
+				.accessDeniedPage("/acesso-negado");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
+	}
+
+}
